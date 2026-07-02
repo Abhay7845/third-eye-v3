48f32381064105aa3f5ec61b5c4d2d7be5d3ff10
Abhay Kumar (Iteanz) <iteanzabhaykumar@titan.co.in>
2026-07-02 12:44:35 +0530

    updated uat code

--- src/HostManger/API/HostUrl.jsx
+++ src/HostManger/API/HostUrl.jsx
@@ -2,8 +2,8 @@
 // export const HOST_URL = "http://localhost:8080";
 
 // UAT TESTING URL WITHOUT SSO
-// export const HOST_URL =
-//   "https://uat-tanishqdigitalmerch.titan.in:8443/ThirdEyeAppTest";
+export const HOST_URL =
+  "https://uat-tanishqdigitalmerch.titan.in:8443/ThirdEyeAppTest";
 
 // UAT TESTING URL WITH SSO
 
@@ -11,4 +11,4 @@
 //   "https://uat-tanishqdigitalmerch.titan.in:8443/ThirdEyeApp";
 
 // PRODUCTION URL
-export const HOST_URL = "https://tanishqdigitalmerch.titan.in:8443/ThirdEyeApp";
+// export const HOST_URL = "https://tanishqdigitalmerch.titan.in:8443/ThirdEyeApp";
