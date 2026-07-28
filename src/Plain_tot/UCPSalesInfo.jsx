05106849a3053be6f8898dc988191e507d107e90
Abhay Kumar (Iteanz) <iteanzabhaykumar@titan.co.in>
2026-07-28 12:39:37 +0530

    updated

--- src/Plain_tot/UCPSalesInfo.jsx
+++ src/Plain_tot/UCPSalesInfo.jsx
@@ -6,12 +6,10 @@ const UCPSalesInfo = ({ data, columns, title }) => {
     <div className='tot_table_container'>
       <div className='tot_table_card'>
         <h6 className='tot_table_title'>{title}</h6>
-
         <table className='tot_sales_table'>
           <thead>
             <tr>
               <th>Category</th>
-
               {columns.map((year) => (
                 <th key={year}>{year}</th>
               ))}
@@ -21,20 +19,25 @@ const UCPSalesInfo = ({ data, columns, title }) => {
           <tbody>
             {data.map((item) => (
               <tr key={item.category}>
-                <td className='tot_category'>{item.category}</td>
-
-                {columns.map((year) => (
-                  <td key={year}>{item[year]}</td>
-                ))}
+                <td className='tot_category'>{item?.category}</td>
+                <td>{item?.Yr1}</td>
+                <td>{item?.Yr2}</td>
+                <td>{item?.Yr3}</td>
+                <td>{item?.Yr4}</td>
+                <td>{item?.Yr5}</td>
+                <td>{item?.Yr6}</td>
               </tr>
             ))}
 
             {title === "UCP Sales" && (
               <tr className='btq_rate_row'>
-                <td className='tot_category'>Btq rate</td>
-                {columns.map((year) => (
-                  <td key={year}>11760</td>
-                ))}
+                <td className='tot_category'>Btq Rate</td>
+                <td>11760</td>
+                <td>11760</td>
+                <td>11760</td>
+                <td>11760</td>
+                <td>11760</td>
+                <td>11760</td>
               </tr>
             )}
           </tbody>
