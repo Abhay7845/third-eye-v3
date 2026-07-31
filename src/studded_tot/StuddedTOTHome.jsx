acff248a0d44ebe5f9ec285fb711d49b079563f0
Abhay Kumar (Iteanz) <iteanzabhaykumar@titan.co.in>
2026-07-31 10:59:34 +0530

    updated

--- src/studded_tot/StuddedTOTHome.jsx
+++ src/studded_tot/StuddedTOTHome.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import "./studdedtotstyle/StuddedTOTHome.css";
 import ThirdEyeHeader from "../Components/custom/ThirdEyeHeader";
+import { studded_tot } from "./studdedtotDummy";
+import StuddedTOTYearwiseTbl from "./StuddedTOTYearwiseTbl";
 
 const StuddedTOTHome = () => {
   return (
     <React.Fragment>
       <ThirdEyeHeader />
-      StuddedTOTHome
+      <div className='studded_tot_yearwise_tbl_layout'>
+        <StuddedTOTYearwiseTbl
+          data={studded_tot.studded_nsv_y1_6}
+          columns={studded_tot.columns}
+        />
+
+        <StuddedTOTYearwiseTbl
+          data={studded_tot.studded_nsv_y1_6}
+          columns={studded_tot.columns}
+        />
+
+        <StuddedTOTYearwiseTbl
+          data={studded_tot.studded_nsv_y1_6}
+          columns={studded_tot.columns}
+        />
+
+        <StuddedTOTYearwiseTbl
+          data={studded_tot.studded_nsv_y1_6}
+          columns={studded_tot.columns}
+        />
+      </div>
     </React.Fragment>
   );
 };
---
+++ src/studded_tot/StuddedTOTYearwiseTbl.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "../studded_tot/studdedtotstyle/StuddedTOTYearwiseTbl.css";
+
+const StuddedTOTYearwiseTbl = ({ data, columns }) => {
+  const columnKeys = [
+    "gis",
+    "regular",
+    "colorStones",
+    "solitaireAUnder70C",
+    "solitaireB70To100C",
+    "solitaireC1CrtPlus",
+    "solitaireD2CrtPlus",
+    "total",
+  ];
+  return (
+    <div className='StuddedTOTYearwiseTbl'>
+      <div className='StuddedTOTYearwiseTbl__wrapper'>
+        <table className='StuddedTOTYearwiseTbl__table'>
+          <thead className='StuddedTOTYearwiseTbl__thead'>
+            <tr>
+              <th className='StuddedTOTYearwiseTbl__metric'>Metric</th>
+
+              {columns.map((item) => (
+                <th key={item} className='StuddedTOTYearwiseTbl__heading'>
+                  {item}
+                </th>
+              ))}
+            </tr>
+          </thead>
+
+          <tbody>
+            {data.map((row, index) => (
+              <tr
+                key={row.metric}
+                className={`StuddedTOTYearwiseTbl__row ${
+                  index % 2 ? "StuddedTOTYearwiseTbl__row--even" : ""
+                }`}>
+                <td className='StuddedTOTYearwiseTbl__metric'>{row.metric}</td>
+
+                {columnKeys.map((key) => (
+                  <td key={key} className='StuddedTOTYearwiseTbl__cell'>
+                    {row[key] ?? "-"}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default StuddedTOTYearwiseTbl;
